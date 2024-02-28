35b8908663d2759b5a9e972a52271af56aadea73
KwonBumcheol <bumcheol1@gmail.com>
2024-02-28 23:18:09 +0900

    24.02.28

---
+++ JavaScript/momentum/app.js
@@ -0,0 +1,9 @@
+const a = 5;
+const b = 2;
+
+const myName = "Kwon"; // variable name에는 공백X 대문자 사용 -> py "_" 사용
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
