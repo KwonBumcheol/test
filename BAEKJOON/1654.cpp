77f50149eb19cac8e6c550140b92c541db8560ec
KwonBumcheol <bumcheol1@gmail.com>
2024-06-14 00:28:41 +0900

    24.06.13

--- BAEKJOON/1654.cpp
+++ BAEKJOON/1654.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 
+unsigned int K, N; // 오영식 가지고 있는 랜선의 개수 K, 필요한 랜선의 수 N
+unsigned int list[10000];
+unsigned int ans;
+
 int main(){
-    int K, N, sum = 0; // 오영식 가지고 있는 랜선의 개수 K, 필요한 랜선의 수 N
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
     cin >> K >> N;
 
-    int num[1000000];
+    unsigned int maxi = 0;
 
     for(int i = 0; i < K; i++){
-        cin >> num[i];
-        sum += num[i];
+        cin >> list[i];
+        maxi = max(maxi, list[i]);
     }
-    sum = sum / N;
 
-    cout << sum;
+    unsigned int left = 1, right = maxi, mid;
+    
+    while(left <= right){
+        mid = (left + right) / 2;
+        unsigned int now = 0;
 
-    return 0;
+        for(int i = 0; i < K; i++){
+            now += list[i] / mid;
+        }
+
+        if(now >= N){
+            left = mid + 1;
+            ans = max(ans, mid);
+        } else {
+            right = mid - 1;
+        }
+    }
+    cout << ans << '\n';
 }
